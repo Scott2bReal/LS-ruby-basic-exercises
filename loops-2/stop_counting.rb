df09c0a871ff8abf164a334805818807c21e4191
Scott Hoecker <hoeckers@gmail.com>
2021-07-16 11:10:17 -0500

    finished up loops 2

--- loops-2/stop_counting.rb
+++ loops-2/stop_counting.rb
@@ -5,3 +5,23 @@
   puts index
   break if index == 2
 end
+
+# Further Exploration
+#
+# Without running this code, how many values will be printed?
+
+5.times do |index|
+  puts index
+  break if index == 4
+end
+
+# 5 values, (0..4)
+
+# How many values will this code print?
+
+5.times do |index|
+  puts index
+  break if index < 7
+end
+
+# it will only print one value, because index will be < 7 on the first iteration
