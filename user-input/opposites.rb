d168de0fab5bf854e9fa3a14eca66079d73228ed
Scott Hoecker <hoeckers@gmail.com>
2021-07-28 17:10:37 -0500

    Cleaned up style on opposites.rb

--- user-input/opposites.rb
+++ user-input/opposites.rb
@@ -2,10 +2,10 @@
 # and then displays the result. Furthermore, insist that one of the integers be
 # positive, and one negative; however, the order in which the two integers are
 # entered does not matter.
-# 
+#
 # Do not check for the positive/negative requirement until both integers are
 # entered, and start over if the requirement is not met.
-# 
+#
 # You may use the following method to validate input integers:
 
 def validate_number?(number_string)
@@ -14,10 +14,10 @@ end
 
 def number_getter
   loop do
-    print "Please enter a positive or negative integer: "
+    print 'Please enter a positive or negative integer: '
     number = gets.chomp
-    if !validate_number?(number)
-      puts "Invalid input. Only non-zero integers are allowed."
+    unless validate_number?(number)
+      puts 'Invalid input. Only non-zero integers are allowed.'
       next
     end
     return number
@@ -30,7 +30,8 @@ number2 = nil
 loop do
   number1 = number_getter
   number2 = number_getter
-  break if number1.to_i / number2.to_i < 0
+  break if (number1.to_i / number2.to_i).positive?
+
   puts "Sorry. One integer must be positive, one must be negative.\nPlease start over."
 end
 
